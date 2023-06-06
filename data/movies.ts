be794b0a0a5d78485f92ddc5396034085e366f3b
gastonmatias <g.matiasvillagra@gmail.com>
2023-06-06 17:40:47 -0400

    REFACTOR - the on-theathers section must have at least 5 movies, when home page is mounted

---
+++ data/movies.ts
@@ -0,0 +1,39 @@
+import { IMovie } from "interfaces/movie";
+
+export const moviesOnTheathers: IMovie[] = [
+    {
+      id:1,
+      title: 'Spider-man: Across the Spider Verse',
+      name: 'spiderman_across',
+      url:   '/movies/spiderman-across',
+      poster1: 'https://cdn.mallmarina.cl/mallbarrio/uploads/2023/05/Spiderman-460-x-660-1.jpg'
+    },
+    {
+      id:2,
+      title: 'Spider-man: Across the Spider Verse',
+      name: 'spiderman_across',
+      url:   '/movies/spiderman-across',
+      poster1: 'https://cdn.mallmarina.cl/mallbarrio/uploads/2023/05/Spiderman-460-x-660-1.jpg'
+    },
+    {
+      id:3,
+      title: 'Spider-man: Across the Spider Verse',
+      name: 'spiderman_across',
+      url:   '/movies/spiderman-across',
+      poster1: 'https://cdn.mallmarina.cl/mallbarrio/uploads/2023/05/Spiderman-460-x-660-1.jpg'
+    },
+    {
+      id:4,
+      title: 'Spider-man: Across the Spider Verse',
+      name: 'spiderman_across',
+      url:   '/movies/spiderman-across',
+      poster1: 'https://cdn.mallmarina.cl/mallbarrio/uploads/2023/05/Spiderman-460-x-660-1.jpg'
+    },
+    {
+      id:5,
+      title: 'Spider-man: Across the Spider Verse',
+      name: 'spiderman_across',
+      url:   '/movies/spiderman-across',
+      poster1: 'https://cdn.mallmarina.cl/mallbarrio/uploads/2023/05/Spiderman-460-x-660-1.jpg'
+    },
+  ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { Layout } from '@/components/layouts/Layout'
+import { moviesOnTheathers } from '@/data/movies'
 
 export default function Home() {
-  
+
   return (
     <>
     <Layout content={'cinema'}>
@@ -12,11 +13,19 @@ export default function Home() {
           <h1 className='text-3xl bg-neutral py-2 font-bold shadow-lg'>On Theathers</h1>
         </div>
   
-        <div id='onTheathers' className='grid grid-cols-2 sm:grid-cols-5 gap-2'>
-          
-
-   
-
+        <div data-testid='onTheathers' className='grid grid-cols-2 sm:grid-cols-5 gap-2'>
+          {
+            moviesOnTheathers.map(e => (
+              <div key={e.id} className="w-fit bg-base-100 shadow-xl border border-gray-500">{/* con class "card" se buguea */}
+                <figure>
+                  <img src={e.poster1} alt={e.title} />
+                </figure>
+                <button className='btn btn-xs btn-info btn-block rounded-none'>
+                  See More
+                </button>
+              </div>
+            ))
+          }
         </div> {/* END On Theathers -------------------------------------------- */}
 
         <div className="divider"></div> 
