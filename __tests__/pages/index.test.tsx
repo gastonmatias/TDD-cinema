946981902db05225ef5d0bf07a8d88e6bcbcf6dc
gastonmatias <g.matiasvillagra@gmail.com>
2023-06-06 16:19:49 -0400

    RED - when home is mounted, the nav should have the appropiate links

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/pages/index'
+
+describe('When home is mounted, the navbar', () => {
+  
+  // GUS_1
+  test('should have the links to: on theathers, coming soon & about', () => {
+    
+    render(<Home />)
+
+    // identifica navbar
+    const navElement = screen.getByRole('navigation'); // Obtener el elemento <nav>
+    
+    // identifica links
+    const onTheathersLink = screen.queryByText(/On Theathers/i, { selector: 'a[id="a-ontheathers-desktop"]' }); // Buscar el elemento <a> por el texto y el id
+    const comingSoonLink  = screen.queryByText(/Coming Soon!/i, { selector: 'a[id="a-comingsoon-desktop"]' }); 
+    const aboutLink       = screen.queryByText(/About/i,        { selector: 'a[id="a-about-desktop"]' }); 
+  
+    // Verificar que el elemento <a> está dentro del elemento <nav>
+    expect(onTheathersLink?.closest('nav')).toBe(navElement); 
+    expect(comingSoonLink?. closest('nav')).toBe(navElement); 
+    expect(aboutLink?.      closest('nav')).toBe(navElement);      
+
+  })
+})
